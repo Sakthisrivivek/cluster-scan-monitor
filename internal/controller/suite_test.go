c66a228b774eaca62ea6747e0cdf27f85b63a57c
Sakthisrivivek <90056891+Sakthisrivivek@users.noreply.github.com>
2024-05-23 01:35:48 -0400

    Update suite_test.go

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -32,7 +32,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	batchv1alpha1 "github.com/clusterscan/clusterscan-operator/api/v1alpha1"
+	batchv1alpha1 "github.com/sakthisrivivek/cluster-scan-monitor/api/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
 
