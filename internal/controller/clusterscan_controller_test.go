0af5f6ce6055722e7e94ad0106d680a5bd5a7eb8
Sakthisrivivek <sakthipanchabakesan98@gmail.com>
2024-05-23 22:07:25 -0400

    final changes

--- internal/controller/clusterscan_controller_test.go
+++ internal/controller/clusterscan_controller_test.go
@@ -38,7 +38,7 @@ var _ = Describe("ClusterScan Controller", func() {
 
 		typeNamespacedName := types.NamespacedName{
 			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
+			Namespace: "default", 
 		}
 		clusterscan := &batchv1alpha1.ClusterScan{}
 
@@ -51,14 +51,14 @@ var _ = Describe("ClusterScan Controller", func() {
 						Name:      resourceName,
 						Namespace: "default",
 					},
-					// TODO(user): Specify other spec details if needed.
+					
 				}
 				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 			}
 		})
 
 		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			
 			resource := &batchv1alpha1.ClusterScan{}
 			err := k8sClient.Get(ctx, typeNamespacedName, resource)
 			Expect(err).NotTo(HaveOccurred())
@@ -77,8 +77,7 @@ var _ = Describe("ClusterScan Controller", func() {
 				NamespacedName: typeNamespacedName,
 			})
 			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		
 		})
 	})
 })
